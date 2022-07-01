a387990197510d938cd6dfbfa5edc836bcda8ae5
cherced <Chernandezcediel@gmail.com>
2022-07-01 09:08:43 -0500

    last Styles Detail for mobile until fold and normal screen

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "bb4ee3563e5fcf46"; }
+/******/ 		__webpack_require__.h = function() { return "9999293c4e18eec7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
