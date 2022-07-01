da78f922498c6f10f47ba5638573c181f6ac9581
cherced <Chernandezcediel@gmail.com>
2022-07-01 08:43:36 -0500

    newQueries for tablets

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5ca9b9786ab1929d"; }
+/******/ 		__webpack_require__.h = function() { return "bb4ee3563e5fcf46"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
