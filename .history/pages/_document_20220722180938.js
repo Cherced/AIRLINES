4797acba2e72f2297b07e44ad556ea07c6aebdb8
cherced <Chernandezcediel@gmail.com>
2022-07-22 18:46:20 -0500

    Skip-link and smooth behaivor

---
+++ .history/pages/_document_20220722180938.js
@@ -0,0 +1,28 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+// extructura de documento para enunciar metadatos y un portal de nodo externo para lanzar el modal de idiomas
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en" >
+        <Head >
+          <div>
+          <title>AIRLINE || CHERCED</title>
+          <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet" />  
+            <meta charSet="UTF-8" />
+            <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <meta name="description" content="Tecnical Prube For NewShore.es" />
+          </div>          
+        </Head>
+        <body>
+          <a href="#main" className="skip-link">Saltar al contenido</a>
+          <Main/>
+          <div id="myportal"></div>
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "375a0eaa839223c2"; }
+/******/ 		__webpack_require__.h = function() { return "82ffad23a76472fd"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
